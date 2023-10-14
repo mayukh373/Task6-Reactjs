ac4a99229cf2fd64eadf09703d47bdc7e8613d7d
Mayukh <mayukhmaji373@gmail.com>
2023-10-15 01:56:21 +0530

    upd1

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,12 @@
 import cpp from './images/cpp.png'
 import java from './images/java.jpg'
-import JS from './images/JS.png'
+import JS from './images/JS.jpg'
+import BS from './images/BS.jpg'
+import React from './images/React.jpg'
+import html from './images/html.jpg'
+import CSS from './images/CSS.jpg'
+import Node from './images/Node.jpg'
+import mongo from './images/mongo.jpg'
 
 export default function Skills() {
     return (
@@ -16,7 +22,7 @@ export default function Skills() {
                                     <div className="text-center my-4"><img src={cpp} alt="cpp.png" height={60} /></div>
                                     <div className="d-flex justify-content-evenly">
                                         <div><img src={java} alt="java.jpg" height={60} /></div>
-                                        <div><img src={JS} alt="JS.jpg" height={60}/></div>
+                                        <div><img src={JS} alt="JS.jpg" height={60} /></div>
                                     </div>
                                 </div>
                             </div>
@@ -27,11 +33,14 @@ export default function Skills() {
                             <div className="flip-inner">
                                 <div className="flip-front"><h5 className="front-text-2">Frontend</h5></div>
                                 <div className="flip-back">
-                                    <ul className="flip-back-list">
-                                        <li>Java</li>
-                                        <li>CPP</li>
-                                        <li>Javascript</li>
-                                    </ul>
+                                    <div className="d-flex justify-content-evenly mt-4">
+                                        <div><img src={html} alt="java.jpg" height={60} /></div>
+                                        <div><img src={CSS} alt="JS.jpg" height={60} /></div>
+                                    </div><br />
+                                    <div className="d-flex justify-content-evenly">
+                                        <div><img src={BS} alt="java.jpg" height={60} /></div>
+                                        <div><img src={React} alt="JS.jpg" height={60} /></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -41,11 +50,10 @@ export default function Skills() {
                             <div className="flip-inner">
                                 <div className="flip-front"><h5 className="front-text-2">Backend</h5></div>
                                 <div className="flip-back">
-                                    <ul className="flip-back-list">
-                                        <li>Java</li>
-                                        <li>CPP</li>
-                                        <li>Javascript</li>
-                                    </ul>
+                                    <div className="d-flex justify-content-evenly mt-5 pt-3">
+                                        <div><img src={Node} alt="java.jpg" height={60} /></div>
+                                        <div><img src={mongo} alt="JS.jpg" height={60} /></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
